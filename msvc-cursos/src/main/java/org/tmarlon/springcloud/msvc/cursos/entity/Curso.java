30eaf25e76dec1ba2e32b75b2870b502e3f8b6b6
Marlon Tarrillo <marlontarrillo19@gmail.com>
2024-07-22 17:52:23 -0500

    creamos la clase intermediaria cursoUsuario

--- msvc-cursos/src/main/java/org/tmarlon/springcloud/msvc/cursos/entity/Curso.java
+++ msvc-cursos/src/main/java/org/tmarlon/springcloud/msvc/cursos/entity/Curso.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @Entity
 @Table(name = "cursos")
 public class Curso {
@@ -14,6 +18,16 @@ public class Curso {
 
     @NotBlank
     private String nombre;
+
+    //un solo curso puede tener muchos usuarios
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<CursoUsuario> cursoUsuarios;
+
+    //creamos un instancia para cursos_usuarios gracias a un constructor vacio
+    public Curso() {
+        cursoUsuarios= new ArrayList<>();
+    }
+
 
     public Long getId() {
         return id;
@@ -30,4 +44,32 @@ public class Curso {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public List<CursoUsuario> getCursoUsuarios() {
+        return cursoUsuarios;
+    }
+
+    public void setCursoUsuarios(List<CursoUsuario> cursoUsuarios) {
+        this.cursoUsuarios = cursoUsuarios;
+    }
+
+    //metodos para agregar un usuario y eliminar un usuario
+
+    //agrega
+    public void  addCursoUsuario( CursoUsuario  cursoUsuario  ){
+
+        this.cursoUsuarios.add( cursoUsuario);
+
+    }
+    //alimina
+    public void  deleteCursoUsuario( CursoUsuario cursoUsuario ){
+        //cuando lo eliminemos comparara por el id del usuario
+        //esto es gracias al equals del cursoUsuario
+        this.cursoUsuarios.remove(cursoUsuario );
+
+    }
+
+
+
+
 }
---
+++ msvc-cursos/src/main/java/org/tmarlon/springcloud/msvc/cursos/entity/CursoUsuario.java
@@ -0,0 +1,56 @@
+package org.tmarlon.springcloud.msvc.cursos.entity;
+
+
+import jakarta.persistence.*;
+
+//tabla intermedia registra los id de los usuarios de un curso en particular
+@Entity
+@Table(name = "cursos_usuarios")
+public class CursoUsuario {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    //crea un indice unico (no habra el mismo usuario en el curso )
+    @Column(name = "usuario_id" ,unique = true)
+    private Long usuarioId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        //si este objeto es igual al objeto pasado
+        if ( this == o ){
+            return true;//retornamos true
+        }
+        //si el objeto  no es una instancia del cursUsuario
+        if (!(o instanceof CursoUsuario)  ){
+            return false;//retornamos false
+        }
+
+        //el objeto sera igual cuando comparemos con los id del usuario
+        CursoUsuario obj = (CursoUsuario) o;
+        return this.usuarioId != null && this.usuarioId.equals( obj.usuarioId );
+
+    }
+
+
+
+}
